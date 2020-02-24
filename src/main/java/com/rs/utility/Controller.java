24e57e60e89c3e916ee08c08bc2ba9fdc02682da
sameershaik <sameershaik03@gmail.com>
2020-02-24 13:37:51 +0530

    Add files via upload

--- src/main/java/com/rs/utility/Controller.java
+++ src/main/java/com/rs/utility/Controller.java
@@ -6,10 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/")
@@ -27,8 +28,22 @@ public class Controller {
     @PostMapping("exclude")
     public ResponseEntity<?> exclude(@RequestBody Object inputObject) throws JsonProcessingException {
         LOGGER.info("excludeUEObject {}", mapper.writeValueAsString(inputObject));
-        return new ResponseEntity<>(inputObject, HttpStatus.OK);
+        Map<String,Object> map=new HashMap<>();
+        map.put("Keys",new ArrayList<>());
+        return new ResponseEntity<>(map, HttpStatus.OK);
     }
 
-
+    @GetMapping("get")
+    @CrossOrigin("*")
+    public Map<String,Object> get(){
+        Map<String,Object> map=new HashMap<>();
+        map.put("Pick notification",10);
+        map.put("Pack notification",20);
+        map.put("sorting",30);
+        map.put("DO updates",40);
+        map.put("inventory sync",40);
+        map.put("inventory adjustment",40);
+        map.put("ASN",40);
+        return map;
+    }
 }
